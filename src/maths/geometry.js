4bbaaba8b43f66483dfd2176bac79f5d8d1252e1
andrezero <andre.torgal@gmail.com>
2018-04-24 16:41:38 +0200

    add Geometry.getAABBRect()

--- src/maths/geometry.js
+++ src/maths/geometry.js
@@ -7,12 +7,38 @@ const modulo = (dividend, divisor) => {
   return (dividend % divisor + divisor) % divisor;
 };
 
-const normalizeHalfAngle = function (angle) {
+const normalizeHalfAngle = (angle) => {
   return modulo(angle + Math.PI, Math.PI * 2) - Math.PI;
 };
 
-const normalizeAngle = function (angle) {
+const normalizeAngle = (angle) => {
   return modulo(angle, Math.PI * 2);
+};
+
+const getAABBRect = (points) => {
+  const length = points.length;
+  const min = {
+    x: points[0].x,
+    y: points[0].y
+  };
+  const max = {
+    x: points[0].x,
+    y: points[0].y
+  };
+  for (let ix = 1; ix < length; ix++) {
+    var point = points[ix];
+    if (point.x < min.x) {
+      min.x = point.x;
+    } else if (point.x > max.x) {
+      max.x = point.x;
+    }
+    if (point.y < min.y) {
+      min.y = point.y;
+    } else if (point.y > max.y) {
+      max.y = point.y;
+    }
+  }
+  return [min.x, min.y, max.x - min.x, max.y - min.y];
 };
 
 const radiansDelta = (radiansA, radiansB) => {
@@ -30,6 +56,7 @@ const rotateVector = (vector, radians, origin) => {
 };
 
 const Geometry = {
+  getAABBRect,
   radiansToVector,
   radiansDelta,
   normalizeHalfAngle,
